adfc3bb6bc721b31d2d935eafa83978b2ecc3a98
Guy Carver <gcarver@bethsoft.com>
2018-05-19 16:42:50 -0400

    Adding wip stuff.

---
+++ projects/wip/scratch.py
@@ -0,0 +1,12 @@
+#Just some scratch stuff.
+
+#Get input from joystick.
+
+#Value starts at 0 with a rate of change and a min/max range.
+#Scale the input to a % of the rate.
+#Add that rate to the value every frame.
+
+
+
+
+
