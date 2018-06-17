2ebfb144a4b3e972e24dd080f40b9a823cae02a7
Guy Carver <gcarver@bethsoft.com>
2018-06-17 00:25:38 -0400

    Added bounds calculation.  Though we aren't currently using it.

--- projects/sentrybot/angle.py
+++ projects/sentrybot/angle.py
@@ -15,3 +15,14 @@ def cossin( aAngle ) :
   '''Return (cos, sin) of given angle in degrees'''
   aAngle = radians(aAngle)
   return (cos(aAngle), sin(aAngle))
+
+def calcbounds( aAngle, xs, ys ):
+  '''Calculate the virtual bounds given an angle and servo x,y min/max ranges.'''
+  cs, sn = cossin(aAngle)
+  x0 = xs[0] * cs
+  x1 = xs[1] * cs
+  y0 = ys[0] * (1.0 - sn)
+  y1 = ys[1] * (1.0 - sn)
+#  print(y0, y1)
+
+  return (x0, x1), (y0, y1)
