d3f0815df71c5fcb8048344c93a4abfa4ea1e956
Guy Carver <gcarver@bethsoft.com>
2021-04-05 16:11:16 -0400

    Converted to use an interrupt to read the falling edge of the echo sensor so we can do work while waiting for the value.

--- projects/sharedlibs/hcsr04/__init__.py
+++ projects/sharedlibs/hcsr04/__init__.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 #driver for the HC-SR04 sonic distance sensor.
+#Call update to cause the sensor to read a value.
+#Get that value with gettime(). It's best to separate these
+# 2 calls by enough time to allow the read to occur. This could
+# be as much as 38ms.
 
 from ctypes import CDLL, POINTER, c_void_p, c_uint
 
@@ -17,8 +21,12 @@ def release( aInstance ):
 
 #--------------------------------------------------------
 def update( aInstance ):
-  '''Get delay in microseconds from echo.'''
-  return _lib.Update(aInstance)
+  ''' Update the value on the echo. '''
+  _lib.Update(aInstance)
 
+#--------------------------------------------------------
+def gettime( aInstance ):
+  ''' Read the time value from the echo. '''
+  return _lib.Read(aInstance)
 
 
--- projects/sharedlibs/hcsr04/hcsr04lib.cpp
+++ projects/sharedlibs/hcsr04/hcsr04lib.cpp
@@ -31,13 +31,22 @@
 // DATE    04/05/2021 09:41 AM
 //----------------------------------------------------------------------
 
+#include <cstdlib>
 #include <cstdint>
 #include <wiringPi.h>
 
-// #define DEBUGOUT 1
+//#define DEBUGOUT 1
 #ifdef DEBUGOUT
 #include <iostream>
 #endif //DEBUGOUT
+
+//NOTE: Interrupt doesn't work with 3 pin mode. Switching the pin to output
+// messes up the interrupt system.  I attempted to reset it but that had no affect.
+
+//The winringPi interrupt callback system doesn't have a payload, so we have
+// to supply a unique function for each pin. This is the signature of the
+// interrupt callback function.
+typedef void (*cbFunction)(  );
 
 //--------------------------------------------------------
 // Reads the distance from the echo pin, however it waits in a tight loop.
@@ -55,12 +64,18 @@ public:
 	{
 		pinMode(Echo, INPUT);
 		pullUpDnControl(Echo, PUD_DOWN);
+
+		InterruptA[aEcho] = this;
+
 		pinMode(Trigger, OUTPUT);
 		digitalWrite(Trigger, LOW);
+
+		//Set the ISR function to trigger on FALLING (Pin goes to 0).
+		wiringPiISR(aEcho, INT_EDGE_FALLING, callbacks[aEcho]);
 	}
 
 	//--------------------------------------------------------
-	uint32_t Update(  )
+	void Update(  )
 	{
 		//Trigger the signal.
 		digitalWrite(Trigger, HIGH);
@@ -73,39 +88,32 @@ public:
 		}
 
 		//Now wait for the return.
-		auto start = micros();
-		int32_t res;
+		Start = micros();
+		bWaiting = true;						// Set waiting for interupt.
+	}
 
-		//Wait for the pin to go high to indicate distance sensing has started.
-		do
-		{
-			res = digitalRead(Echo);
-		} while(res == LOW);
+	//--------------------------------------------------------
+	//Callback to read the time at falling edge of pin change.
+	void SetTime(  )
+	{
+		if (bWaiting) {
+			bWaiting = false;
 
-		//Now wait for timout or for pin to go low to get distance.
-		for ( uint32_t count = 0; (res == HIGH) && (count < 50000); ++count) {
-			res = digitalRead(Echo);
+			uint32_t dt = micros() - Start;
+
+			//If msb is set, the counter wrapped around so subtract from 0 to get the right value.
+			// Can't just negate dt because it's an unsigned value.
+			if (dt & 0x80000000) {
+				dt = 0 - dt;
+			}
+
+			//If in 3 pin mode, set the pin back to output.
+			if (b3Pin) {
+				pinMode(Trigger, OUTPUT);
+			}
+
+			Time = dt;
 		}
-		auto end = micros();
-
-		//If in 3 pin mode, set the pin back to output.
-		if (b3Pin) {
-			pinMode(Trigger, OUTPUT);
-		}
-
-		auto dt = end - start;
-
-		//If msb is set, the counter wrapped around so subtract from 0 to get the right value.
-		// Can't just negate dt because it's an unsigned value.
-		if (dt & 0x80000000) {
-			dt = 0 - dt;
-		}
-
-#ifdef DEBUGOUT
-		std::cout << "Time: " << dt << std::endl;
-#endif //DEBUGOUT
-
-		return dt;
 	}
 
 	//--------------------------------------------------------
@@ -117,14 +125,71 @@ public:
 		}
 	}
 
+	uint32_t QTime(  ) const { return Time; }
+
 private:
 	uint32_t Trigger;
 	uint32_t Echo;
+	uint32_t Time = 0;
+	uint32_t Start = 0;
 	bool b3Pin;									// Indicate if in 3 pin mode.
+	bool bWaiting = false;						// When True, waiting for interrupt.
 	static bool bInitialized;					// Indicates if wiringPi has been initialized.
+	static hcsr04 *InterruptA[32];				// Array of hcsr04 objects by pin.
+	static cbFunction callbacks[];				// Array of unique interrupt callback functions.
+
+	//--------------------------------------------------------
+	template<uint32_t P> static void callback(  )
+	{
+#ifdef DEBUGOUT
+		std::cout << "callback" << P << std::endl;
+#endif //DEBUGOUT
+		hcsr04 *phc  = InterruptA[P];
+		if (phc) {
+			phc->SetTime();
+		}
+	}
+
 };
 
 bool hcsr04::bInitialized = false;
+hcsr04 *hcsr04::InterruptA[] = { nullptr };
+
+//--------------------------------------------------------
+cbFunction hcsr04::callbacks[] = {
+	hcsr04::callback<0>,
+	hcsr04::callback<1>,
+	hcsr04::callback<2>,
+	hcsr04::callback<3>,
+	hcsr04::callback<4>,
+	hcsr04::callback<5>,
+	hcsr04::callback<6>,
+	hcsr04::callback<7>,
+	hcsr04::callback<8>,
+	hcsr04::callback<9>,
+	hcsr04::callback<10>,
+	hcsr04::callback<11>,
+	hcsr04::callback<12>,
+	hcsr04::callback<13>,
+	hcsr04::callback<14>,
+	hcsr04::callback<15>,
+	hcsr04::callback<16>,
+	hcsr04::callback<17>,
+	hcsr04::callback<18>,
+	hcsr04::callback<19>,
+	hcsr04::callback<20>,
+	hcsr04::callback<21>,
+	hcsr04::callback<22>,
+	hcsr04::callback<23>,
+	hcsr04::callback<24>,
+	hcsr04::callback<25>,
+	hcsr04::callback<26>,
+	hcsr04::callback<27>,
+	hcsr04::callback<28>,
+	hcsr04::callback<29>,
+	hcsr04::callback<30>,
+	hcsr04::callback<31>
+};
 
 //Following are the external interface functions.
 extern "C"
@@ -145,10 +210,16 @@ void Release( void *apInstance )
 }
 
 //--------------------------------------------------------
-uint32_t Update( void *apInstance )
+void Update( void *apInstance )
 {
 	auto phcsr = reinterpret_cast<hcsr04*>(apInstance);
 	return phcsr->Update();
+}
+
+uint32_t Read( void *apInstance )
+{
+	auto phcsr = reinterpret_cast<hcsr04*>(apInstance);
+	return phcsr->QTime();
 }
 
 } //extern "C"
